1cbf43ce60d585e5c5105d4b853ee0d94c4ab149
AnastasiiaTetiura <n.tetiura@gmail.com>
2025-02-25 23:25:15 -0800

    introduction file

---
+++ vietnam/welcome_page.py
@@ -0,0 +1,7 @@
+a = print(input("What part of Vitnam you are going to? - "))
+if a == "Northern":
+    print("we are arranging your tour")
+elif a == "Southern":
+    print("Next year")
+else:
+    print("We don't work in that area")
